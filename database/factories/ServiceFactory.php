55034aabfcfe47dffba40905495b16e819492f26
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-10-21 11:34:21 +0700

    Service CRUD, model, table, resource, ext

---
+++ database/factories/ServiceFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Laundry;
+use App\Models\Service;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ServiceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Service::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->words(2, true),
+            'laundry_id' => Laundry::factory()
+        ];
+    }
+}
