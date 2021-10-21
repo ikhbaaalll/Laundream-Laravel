8b0093a803a62ff75c3171df2a64850cda02533c
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-10-21 11:25:34 +0700

    override actingAs with sanctum

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,17 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Contracts\Auth\Authenticatable as UserContract;
+use Laravel\Sanctum\Sanctum;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function actingAs(UserContract $user, $abilities = ['*'])
+    {
+        Sanctum::actingAs($user, $abilities);
+
+        return $this;
+    }
 }
