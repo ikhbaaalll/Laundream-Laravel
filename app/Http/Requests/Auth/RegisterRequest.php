681502015bb71299fb6631ab1ad4d46057cdff6f
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-12-14 16:05:02 +0700

    add RegisterRequest

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class RegisterRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => ['required'],
+            'email' => ['required', Rule::unique('users')],
+            'password' => ['required'],
+            'no_hp' => ['required']
+        ];
+    }
+}
