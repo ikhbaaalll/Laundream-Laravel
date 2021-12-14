333d664f34b7d299f372485dd3aa075c3099392e
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-12-14 16:24:53 +0700

    update config s3, default schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Laundry;
+use App\Observers\LaundryObserver;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +28,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         JsonResource::withoutWrapping();
+        Laundry::observe(LaundryObserver::class);
+        Schema::defaultStringLength(191);
     }
 }
