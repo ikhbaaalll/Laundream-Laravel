7cdf57649949b9c585203ae7a8dc959bda5104e4
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-12-15 07:39:25 +0700

    fix error heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\Laundry;
 use App\Observers\LaundryObserver;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
